44de2960ff291f1c37fcca76e3a778b8ac9a8875
Robert Spencer <rspenc19@uncc.edu>
2024-12-08 01:23:50 -0500

    Enhance AboutPage with detailed personal and professional information, add scroll handling for portrait slider, and improve styling for better presentation.

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -2,30 +2,46 @@
   <div class="content-page">
     <h1><TerminalCursor /> About Me</h1>
     <div class="about-container">
-      <PortraitFrame />
+      <div class="portrait-slider-container">
+        <PortraitFrame />
+      </div>
       <div class="about text-with-portrait">
+        <h2>About Me</h2>
         <p
           v-typewriter="{
             speed: 2,
-            text: 'I\'m Robert, A software engineer based out of Charlotte, North Carolina',
+            text: 'I\'m Robert, A software engineer based out of Charlotte, North Carolina, I currently work at Ezoic as a full stack developer. I have experience with Go, PHP, Python, Javascript (Vue.js), SQL, MongoDB and SKLearn. I\'ve been working as a software engineer for about 3 years, and I\'m always looking for new tech to learn and new projects to work on. I\'m currently focused on backend development and AI/ML, but I have experience with front end development as well.',
+          }"
+        ></p>
+        <h2>Experience</h2>
+        <div class="job-info">
+          <a href="https://www.ezoic.com/" target="_blank">
+            <h3>
+              Ezoic
+            </h3>
+          </a>
+          <img
+            src="https://www.ezoic.com/_vercel/image?url=_astro%2FbigE.8yEvsr1B.png&w=640&q=100"
+            alt="Ezoic Logo"
+            width="30">
+        </div>
+        <p
+          v-typewriter="{
+            speed: 2,
+            text: 'Full-Stack Developer | 2022 - Present',
           }"
         ></p>
         <p
           v-typewriter="{
             speed: 2,
-            text: 'I am a full stack developer, currently working in Go, PHP, Python & Javascript (Vue.js), but interested in all back-end technologies.',
+            text: 'At Ezoic, I tackle diverse projects ranging from internal tools to customer-facing features. This role has allowed me to collaborate across teams, contribute to large-scale systems, and refine my problem-solving skills in a fast-paced environment.',
           }"
         ></p>
+        <h2>Outside of Work</h2>
         <p
           v-typewriter="{
             speed: 2,
-            text: 'I have experience with Torch & Tensorflow, as well as Docker, AWS & Kubernetes',
-          }"
-        ></p>
-        <p
-          v-typewriter="{
-            speed: 2,
-            text: 'I\'m passionate about learning cool new tech, and making fun projects, if you\'d like to see some of them, check out the portfolio section.',
+            text: 'In my spare time, I enjoy playing video games (Mostly CS2), listening to music (Big fan of Peirce the Veil and Seven Lions) and working on my projects. I love finding problems and solving them with a bit of code. Recently, I’ve been exploring ways to combine my interests, developing tools to enhance gameplay or analyze data.',
           }"
         ></p>
       </div>
@@ -36,11 +52,29 @@
 <script>
 import TerminalCursor from "../components/TerminalCursor.vue";
 import PortraitFrame from "@/components/PortraitFrame.vue";
+import { EventBus } from "../eventBus";
 export default {
   name: "AboutPage",
   components: {
     TerminalCursor,
     PortraitFrame,
+  },
+  mounted() {
+    EventBus.on('scroll', 
+      (scrollPercent) => {
+        this.handleScroll(scrollPercent);
+      }
+    );
+  },
+  methods: {
+    handleScroll(scrollPercent) {
+      const portraitFrame = document.querySelector('.portrait-slider-container');
+      if (!portraitFrame) {
+        return;
+      }
+      // This is a funny way to do this, position: fixed; might be better? Doesn't really work with the current layout though.
+      portraitFrame.style.top = `${-15+scrollPercent*28}vh`;
+    }
   },
 };
 </script>
@@ -64,6 +98,33 @@ export default {
   margin-left: 20px;
 }
 
+.about h2, .about h3, .about a {
+  color: #000000;
+  display: inline-block;
+  margin: 10px 0;
+}
+
+.about p{
+  margin: 10px 0;
+}
+
+.about h2:hover, .about h3:hover, .about a:hover {
+  background: linear-gradient(270deg, #F536EE, #F4367D, #F56436);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  animation: flowingGradient 3s ease infinite;
+}
+
+.job-info {
+  display: flex;
+  align-items: center;
+}
+
+.portrait-slider-container {
+  position: relative;
+  top: -15vh;
+}
+
 /* Image looks messed up when the screen is small */
 @media (max-width: 600px) {
   .about-container {
@@ -75,6 +136,10 @@ export default {
     margin-top: 20px;
     mix-blend-mode: difference;
     filter: invert(1);
+  }
+
+  .portrait-slider-container {
+    position: static;
   }
 }
 
